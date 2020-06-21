6fd8149a2f9222401146febfba7f546afbe9095e
Vikrant Bhat <vik.bhat223@gmail.com>
2020-06-22 05:22:27 +0530

    Navbar improved with icons when user is authenticated

--- src/components/EditDetails.js
+++ src/components/EditDetails.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 
 import PropTypes from "prop-types";
 import withStyles from "@material-ui/core/styles/withStyles";
+import MyButton from "../util/MyButton";
 
 // MUI stuff
 import Button from "@material-ui/core/Button";
@@ -10,8 +11,6 @@ import Dialog from "@material-ui/core/Dialog";
 import DialogActions from "@material-ui/core/DialogActions";
 import DialogContent from "@material-ui/core/DialogContent";
 import DialogTitle from "@material-ui/core/DialogTitle";
-import Tooltip from "@material-ui/core/Tooltip";
-import IconButton from "@material-ui/core/IconButton";
 
 // Icons
 import EditIcon from "@material-ui/icons/Edit";
@@ -84,11 +83,13 @@ class EditDetails extends Component {
 		const { classes } = this.props;
 		return (
 			<>
-				<Tooltip title="Edit details" placement="top">
-					<IconButton onClick={this.handleOpen} className={classes.button}>
-						<EditIcon color="primary" />
-					</IconButton>
-				</Tooltip>
+				<MyButton
+					tip="Edit details"
+					onClick={this.handleOpen}
+					btnClassName={classes.button}
+				>
+					<EditIcon color="primary" />
+				</MyButton>
 
 				<Dialog
 					open={this.state.open}
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,29 +1,66 @@
 import React, { Component } from "react";
 import { Link } from "react-router-dom";
+import PropTypes from "prop-types";
+import MyButton from "../util/MyButton";
 
 // MUI stuff
 import AppBar from "@material-ui/core/AppBar";
 import Toolbar from "@material-ui/core/Toolbar";
 import Button from "@material-ui/core/Button";
 
+// Redux stuff
+import { connect } from "react-redux";
+
+// Icons
+import AddIcon from "@material-ui/icons/Add";
+import HomeIcon from "@material-ui/icons/Home";
+import Notifications from "@material-ui/icons/Notifications";
+
 class Navbar extends Component {
 	render() {
+		const { authenticated } = this.props;
 		return (
 			<AppBar>
 				<Toolbar className="nav-container">
-					<Button color="inherit" component={Link} to="/login">
-						Login
-					</Button>
-					<Button color="inherit" component={Link} to="/">
-						Home
-					</Button>
-					<Button color="inherit" component={Link} to="/signup">
-						Signup
-					</Button>
+					{authenticated ? (
+						<>
+							<MyButton tip="Post a scream!">
+								<AddIcon />
+							</MyButton>
+							<Link to="/">
+								<MyButton tip="Home">
+									<HomeIcon />
+								</MyButton>
+							</Link>
+							<MyButton tip="Notifications">
+								<Notifications />
+							</MyButton>
+						</>
+					) : (
+						<>
+							<Button color="inherit" component={Link} to="/login">
+								Login
+							</Button>
+							<Button color="inherit" component={Link} to="/">
+								Home
+							</Button>
+							<Button color="inherit" component={Link} to="/signup">
+								Signup
+							</Button>
+						</>
+					)}
 				</Toolbar>
 			</AppBar>
 		);
 	}
 }
 
-export default Navbar;
+const mapStateToProps = state => ({
+	authenticated: state.user.authenticated,
+});
+
+Navbar.propTypes = {
+	authenticated: PropTypes.bool.isRequired,
+};
+
+export default connect(mapStateToProps)(Navbar);
